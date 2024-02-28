0f7ed535a958f59c0e6443b328612d73ce6a3d20
Elijah <iamngunjiri@gmail.com>
2024-02-28 11:00:08 +0300

    Linked css to webpack and I have the navbar and the header section working properly

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 // index.js
 import { initializePage } from './pageloader.js';
+import './style.css';
 
 // Call the initializePage function when the DOM content is loaded
 document.addEventListener('DOMContentLoaded', () => {
