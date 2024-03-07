ecdc8b6ac15bbadb6e4fd8aed44d3c6c8a3f70d4
Elijah <iamngunjiri@gmail.com>
2024-03-07 09:57:24 +0300

    Done styling the menu page, and it's responsive

--- src/about.js
+++ src/about.js
@@ -2,6 +2,9 @@ export function about() {
     const content = document.getElementById ('content')
     content.classList.add('hidden')
 
+    const menu = document.getElementById('serving')
+    menu.classList.add('hidden')
+
     const about = document.getElementById('about')
     about.classList.remove('hidden')
 }
