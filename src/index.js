c93f500dcf57dda2b1b616d5ccc7db6c688ac01b
Elijah <iamngunjiri@gmail.com>
2024-03-14 15:31:43 +0300

    Finalising the page details

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import {serving} from './serving.js';
 // Call the initializePage function when the DOM content is loaded
 document.addEventListener('DOMContentLoaded', () => {
   initializePage();
-
+  
   document.getElementById('currentYear').textContent = new Date().getFullYear();
 
   const menu = document.querySelector('.fa-bars');
